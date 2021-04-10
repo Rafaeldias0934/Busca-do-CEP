116d0430004f88a78871eb1b1f44c3c589392370
Rafaeldias0934 <74202654+Rafaeldias0934@users.noreply.github.com>
2021-04-10 10:58:30 -0300

    commit

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
 'use strict';
-let preencherformulario = (endereco) => {
-    window.document.getElementById('endereco').value = endereco.logradouro
-    window.document.getElementById('bairro').value = endereco.bairro
-    window.document.getElementById('cidade').value = endereco.localidade
-    window.document.getElementById('estado').value = endereco.uf
+
+const forms = (address) => {
+    document.getElementById('endereco').value = address.logradouro
+    document.getElementById('bairro').value = address.bairro
+    document.getElementById('cidade').value = address.localidade
+    document.getElementById('estado').value = address.uf
 }
-let pesquisarcep = async() => {
-   let cep = window.document.getElementById('cep').value
-   let url = `https://viacep.com.br/ws/${cep}/json/`
-   let dados = await fetch(url)
-   let endereco = await dados.json()
-   if (endereco.hasOwnProperty('erro')) {
-    let emoji = String.fromCodePoint(0x1F447)
+const searchcode = async() => {
+    const code = window.document.getElementById('cep').value
+    const url = `https://viacep.com.br/ws/${code}/json/`
+    const elements = await fetch(url)
+    const address = await elements.json()
+    if (address.hasOwnProperty('erro')) {
+        let emoji = String.fromCodePoint(0x1F447)
         alert(`Por favor, Digite um cep válido no campo ${emoji}`) 
-   } else {
-    preencherformulario(endereco)    
-   }
+    } else {
+        forms(address)    
+    }
 }  
-document.getElementById('cep').addEventListener('focusout',pesquisarcep);
+document.getElementById('cep').addEventListener('focusout',searchcode);
 
 
 
